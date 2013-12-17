2699e9dba4138d6782f07e95f4821eac1eab1f56
daleknauss <daleknauss@gmail.com>
2013-12-16 16:38:45 -0800

    remove detectBigScroll

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -88,7 +88,7 @@ var TableScroller = function (cols, rows) {
 
         self.setCurrentPage(scrollTop)
 
-        self.detectBigScroll(scrollTop);
+        // self.detectBigScroll(scrollTop);
 
         if (downScroll) {
             self.append();
@@ -229,7 +229,7 @@ var TableScroller = function (cols, rows) {
     this.createTbody = function (page) {
         var tbody;
 
-        if (this.reuseableTbodies.length > 1) {
+        if (this.reuseableTbodies.length > 0) {
             tbody = this.reuseTbody(page);
         } else {
             tbody = this.createNewTbody(page);
